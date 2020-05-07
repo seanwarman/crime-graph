10e8ce1fae5052c55dc254b08f3d3e7671587529
Sean <seanwarman@protonmail.com>
2020-05-07 06:34:30 +0100

    rendering bar chart for crime category

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Colourful crime.
 
 - https://dev.to/vish448/create-react-project-without-create-react-app-3goh
 - https://stackoverflow.com/questions/37298215/add-favicon-with-react-and-webpack
+- https://www.robinwieruch.de/learn-react-before-using-redux
+- https://redux.js.org/basics/usage-with-react
+- https://react-redux.js.org/introduction/basic-tutorial
+- https://www.chartjs.org/docs/latest/charts/bar.html
 
 ## State Shape
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchCrimes } from '../actions'
+import { Bar } from 'react-chartjs-2'
+
 
 class Home extends React.Component {
   constructor(props) {
@@ -13,17 +15,63 @@ class Home extends React.Component {
 
   }
 
+  componentDidUpdate() {
+    console.log('this.props.crime: ', this.props.crime)
+
+  }
+
+  labelsByCategory(crime) {
+
+    return crime.reduce((arr,cr) => {
+
+      if(arr.includes(cr.category)) return arr
+      return [ ...arr, cr.category ]
+
+    }, [])
+  }
+
+  dataByCategory(crime) {
+
+    const categories = this.labelsByCategory(crime)
+
+    return categories.map(cat => {
+      return (crime.filter(cr => cr.category === cat) || []).length
+    })
+
+  }
+
   render() {
 
     return (
       <>
-        <ul>
-        {
-          this.props.crime.map(cr =>
-              <li>{cr.category}</li>
-          )
-        }
-      </ul>
+
+      <Bar 
+        data={{
+          labels: this.labelsByCategory(this.props.crime),
+          datasets: [{
+            label: 'Number of Crimes by Category',
+            data: this.dataByCategory(this.props.crime),
+            backgroundColor: 'white',
+            //   'rgba(255, 99, 132, 0.2)',
+            //   'rgba(54, 162, 235, 0.2)',
+            //   'rgba(255, 206, 86, 0.2)',
+            //   'rgba(75, 192, 192, 0.2)',
+            //   'rgba(153, 102, 255, 0.2)',
+            //   'rgba(255, 159, 64, 0.2)'
+            // ],
+            borderColor: 'white',
+            //   'rgba(255, 99, 132, 1)',
+            //   'rgba(54, 162, 235, 1)',
+            //   'rgba(255, 206, 86, 1)',
+            //   'rgba(75, 192, 192, 1)',
+            //   'rgba(153, 102, 255, 1)',
+            //   'rgba(255, 159, 64, 1)'
+            // ],
+            borderWidth: 1
+          }]
+        }}
+      />
+
       </>
 
     )
